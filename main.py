a9fc00c346a1f869114038c196acbf2afb5810e6
NickBritoDev <nicolasbcruz@gmail.com>
2023-05-22 18:42:13 -0300

    feat: criar alarme, dar play em video no youtube, fazer pesquisa no wikipedia

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import re
+from sched import scheduler
+import threading
+import winsound
+import pygame
+import schedule
 import speech_recognition as sr
 import sounddevice as sd
 import wavio as wv
@@ -6,6 +12,13 @@ import webbrowser
 import psutil
 import datetime
 import time
+import wikipedia
+import pywhatkit
+import os
+
+alarme_ativo = False
+ativo = True
+pausado = False
 
 
 def iaFala(fala):
@@ -14,11 +27,73 @@ def iaFala(fala):
     engine.runAndWait()
 
 
-def pesquisaggl(fala):
+def criar_alarme(hora, minutos, arquivo_audio):
+    global alarme_ativo
+    hora = int(hora)
+    minutos = int(minutos)
+
+    alarme = hora * 60 + minutos
+    agora = time.localtime()
+    agora_minutos = agora.tm_hour * 60 + agora.tm_min
+
+    if agora_minutos >= alarme:
+        alarme += 24 * 60
+
+    tempo_restante = alarme - agora_minutos
+
+    print(f"Alarme criado para {hora}:{minutos:02d}.")
+    iaFala(f"Alarme criado para {hora}:{minutos:02d}.")
+    print(
+        f"Tempo restante: {tempo_restante // 60} horas {tempo_restante % 60} minutos."
+    )
+    iaFala(
+        f"Tempo restante: {tempo_restante // 60} horas {tempo_restante % 60} minutos."
+    )
+
+    time.sleep(tempo_restante * 60)
+    pygame.mixer.init()
+    pygame.mixer.music.load(arquivo_audio)
+    pygame.mixer.music.play()
+
+
+def criar_alarme_thread(hora, minutos, arquivo_audio):
+    global alarme_ativo
+    alarme_ativo = True
+    thread = threading.Thread(target=criar_alarme, args=(hora, minutos, arquivo_audio))
+    thread.start()
+
+
+def parar_alarme():
+    global alarme_ativo
+    alarme_ativo = False
+    pygame.mixer.music.stop()
+
+
+def pesquisa_google(fala):
     frase = fala
     search = frase.replace("Pesquisar", "")
     search2 = search.replace("pesquisar", "")
     webbrowser.open(f"https://www.google.com/search?q={search2}")
+
+
+def pesquisa_wikipedia(fala):
+    frase = fala
+    search = frase.replace("procure por", "")
+    wikipedia.set_lang("pt")
+    try:
+        resultado = wikipedia.summary(search, 2)
+        print(resultado)
+        iaFala(resultado)
+    except wikipedia.exceptions.PageError:
+        print("Desculpe, não foram encontrados resultados para a pesquisa.")
+        iaFala("Desculpe, não foram encontrados resultados para a pesquisa.")
+
+
+def pesquisa_youtube(fala):
+    musica = fala.replace("toque", "")
+    pywhatkit.playonyt(musica)
+    iaFala("Tocando...")
+    print("Tocando...")
 
 
 def grava():
@@ -30,12 +105,7 @@ def grava():
     wv.write("minhavoz.wav", gravacao, freq, sampwidth=2)
 
 
-iaFala("Olá sou a Clhoe o que deseja?")
-
-import time
-
-ativo = True
-pausado = False
+iaFala("Olá sou a Chloe o que deseja?")
 
 while True:
     grava()
@@ -51,43 +121,77 @@ while True:
             print("Não foi possível entender a fala")
             fala = ""
 
-    if ativo and not pausado: 
+    # saudação com hora atual
+    if ativo and not pausado:
         if fala == "bom dia" or fala == "boa tarde" or fala == "boa noite":
             iaFala(
                 "Para você também! "
                 "Agora são: " + datetime.datetime.now().strftime("%H:%M")
             )
 
+        # cria um novo alarme
+        if fala.startswith("criar alarme"):
+            parametros = re.findall(r"\d+", fala)
+            if len(parametros) >= 2:
+                hora, minutos = parametros[:2]
+                arquivo_audio = "sound.mp3"
+                criar_alarme_thread(hora, minutos, arquivo_audio)
+            else:
+                iaFala(
+                    "Desculpe, o comando está incorreto. Por favor, especifique a hora e os minutos."
+                )
+                print(
+                    "Desculpe, o comando está incorreto. Por favor, especifique a hora e os minutos."
+                )
+        # deliga o alarme
+        elif fala == "deligar alarme":
+            parar_alarme()
+
+        # pesquisa no google
         word = "pesquisar"
         if fala not in ["bom dia", "boa tarde", "boa noite"]:
             if word in fala:
                 time.sleep(2)
-                pesquisaggl(fala)
+                pesquisa_google(fala)
 
+        # pesquisa no wikipedia
+        if fala.startswith("procure por"):
+            search_query = fala.replace("procure por", "").strip()
+            time.sleep(2)
+            pesquisa_wikipedia(search_query)
+
+        # pesquisa no youtube
+        if fala.startswith("toque"):
+            pesquisa_youtube(fala)
+
+        # executa comando especificos
         if fala == "abrir o youtube":
             webbrowser.open("https://www.youtube.com/")
         elif fala == "abrir o chat gpt":
             webbrowser.open("https://chat.openai.com/")
         elif fala == "abrir o instagram":
             webbrowser.open("https://www.instagram.com/")
+        elif fala == "abrir o facebook":
+            webbrowser.open("https://www.facebook.com/")
 
+        # deliga e fecha o navegador
         powerOff = "sair"
         if fala == powerOff:
             iaFala("Desligando...")
             for proc in psutil.process_iter(["name"]):
                 if "chrome" in proc.info["name"]:
                     proc.kill()
-                if "edge" in proc.info["name"]:
-                    proc.kill()
-                if "firefox" in proc.info["name"]:
-                    proc.kill()
             break
 
+        # pausa os comandos e fica em escuta esperando comando para iniciar
         if fala == "pausar":
             iaFala("Pausando...")
-            pausado = True  
+            print("Pausando...")
+            pausado = True
 
-    elif ativo and pausado:  
+    # reinicia a escuta de comandos
+    elif ativo and pausado:
         if fala == "iniciar":
-            iaFala("Retomando...")
-            pausado = False  
+            iaFala("iniciando...")
+            print("iniciando...")
+            pausado = False
